4eb36ed1bc82da1d6fd1882292acc9c827fe77d0
DevPixel <36967885+HadrianDiaz@users.noreply.github.com>
2018-04-06 13:21:56 -0400

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
-var words = ['computer', 'apple', 'android'];
+var fruitsList = ["computer", "ios", "windows","aids"];
 
 var chosenWord = '';
 
-var lettersIn = [];
+var lettersInChosenWord= [];
 
 var numBlanks = 0;
 
@@ -15,24 +15,96 @@ var letterGuessed = '';
 
 var winCounter = 0;
 var lossCounter = 0;
-var numGuesses = 0;
-
-
-
+var numGuesses = 9;
 
 
 function startGame() {
 
-	numGuesses = 0;
+	numGuesses = 9;
 
-	chosenWord = words[Math.floor(Math.random() * words.legth)];
+	chosenWord = fruitsList[Math.floor(Math.random() * fruitsList.length)];
 
-	lettersIn = chosenWord.split('')
+	lettersInChosenWord = chosenWord.split("");
 
-	numBlanks = lettersIn.legth;
+	numBlanks = lettersInChosenWord.length;
 
 	console.log(chosenWord);
 
-	
+	blanksAndSuccesses = [];
+
+	wrongGuesses = [];
+
+	for (var i = 0; i < numBlanks; i++) {
+		blanksAndSuccesses.push("_");
+	}
+
+	console.log(blanksAndSuccesses);
+
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 }
+
+function checkLetters(letter) {
+	var letterInWord = false;
+
+	for (var i=0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
+			letterInWord = true;
+		}
+	}
+
+
+	if (letterInWord) {
+		for (var j=0; j < numBlanks; j++) {
+			if (chosenWord[j] === letter) {
+				blanksAndSuccesses[j] = letter;
+			}
+		}
+		console.log(blanksAndSuccesses);
+	} 
+
+	else {
+		wrongGuesses.push(letter);
+		numGuesses--;
+	}
+}
+
+function roundComplete() {
+	console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + 
+		" | NumGuesses: " + numGuesses);
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+		winCounter++;
+		alert("You win!");
+
+		document.getElementById("win-counter").innerHTML = winCounter;
+
+		startGame();
+	}
+
+	else if (numGuesses === 0) {
+		lossCounter++;
+		alert("You lose");
+
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+
+		startGame();
+	}
+}
+
+startGame();
+
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+};
