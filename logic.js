e4d51d5ea548da37479a30141f67b64757d28445
Diaz <0223838@students.dadeschools.net>
2018-04-12 13:18:55 -0400

    added more comments

--- logic.js
+++ logic.js
@@ -1,50 +1,51 @@
+// word list (all lower case)
 var fruitsList = ["computer", "ios", "windows","aids"];
-
+// computer selected solution 
 var chosenWord = '';
-
+// breaks the solution into letter to be stored in array
 var lettersInChosenWord= [];
-
+// number of blanks shown based on solution
 var numBlanks = 0;
-
+// holds a mix of blank and solved leters 
 var blanksAndSuccesses = [];
 
 var wrongGuesses = [];
 
 var letterGuessed = '';
 
-
+// game counters
 var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
 
 
 function startGame() {
-
+	// resets the guesses back to 0
 	numGuesses = 9;
-
+	// solution is chosen randomly from wordlist
 	chosenWord = fruitsList[Math.floor(Math.random() * fruitsList.length)];
-
+	// the word is split into letters
 	lettersInChosenWord = chosenWord.split("");
-
+	// counts the number of letters in the word
 	numBlanks = lettersInChosenWord.length;
-
+	// prints the solution in the console (for testing purposes)
 	console.log(chosenWord);
-
+	// resets the guess and success array aat each round
 	blanksAndSuccesses = [];
-
+	// resets the wrong guesses from the previous round
 	wrongGuesses = [];
-
+	// fills up the blanks annd sucesses list with the correct number corspondning to the lenght of the chosen word
 	for (var i = 0; i < numBlanks; i++) {
 		blanksAndSuccesses.push("_");
 	}
-
+	// prints the initial blanks in the console 
 	console.log(blanksAndSuccesses);
 
-
+	// reprints the guesses left
 	document.getElementById("guesses-left").innerHTML = numGuesses;
-
+	// prints the blanks at the beging of each round
 	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-
+	// clears wrong guesses
 	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 }
